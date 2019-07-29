64a45a1d443995622866b590047dd6c1e5cd7100
hworoshch <hworoshch@gmail.com>
2019-07-29 13:45:57 +0700

    19th:rack

---
+++ config.ru
@@ -0,0 +1,4 @@
+require_relative 'app'
+require_relative 'datetime'
+
+run App.new
---
+++ datetime.rb
@@ -0,0 +1,40 @@
+class DateTimeFormat
+  FORMATS = {
+    date: {
+      'year' => '%Y',
+      'month' => '%m',
+      'day' => '%d' },
+    time: {
+      'hour' => '%H',
+      'minute' => '%M',
+      'second' => '%S' }
+  }.freeze
+
+  def initialize(date, format)
+    @date = date
+    @format = format ? format.split(',') : []
+    @wrong = []
+    @converted = []
+    @formatted = @date.strftime(convert_format)
+  end
+
+  def has_wrong?
+    @wrong.any?
+  end
+
+  def to_s
+    self.has_wrong? ? "Unknown format #{@wrong}" : @formatted
+  end
+
+  private
+
+  def check_format(format)
+    new_format = FORMATS[:date][format] || FORMATS[:time][format]
+    new_format ? @converted << new_format : @wrong << format
+  end
+
+  def convert_format
+    @format.each { |f| check_format(f) }
+    @converted.join('-')
+  end
+end
